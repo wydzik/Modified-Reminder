93d8c7547780644a06295ab1a8b1232079c69561
Łukasz Dawydzik <lukasz.dawydzik@student.put.poznan.pl>
2019-11-17 12:59:45 +0100

    Additional reminds before event

--- event.erl
+++ event.erl
@@ -1,52 +1,127 @@
 -module(event).
--compile(export_all).
+-export([start/2, start_link/4, cancel/1]).
+-export([init/5, loop/1]).
 -record(state, {server,
-				name="",
-				to_go=0}).
+                name="",
+                to_go={{1970,1,1},{0,0,0}},
+				num_of_reminds = 0,
+				remind_dates = []}).
 
-loop(S = #state{server=Server, to_go = [T|Next]}) ->
-	receive
-		{Server, Ref, cancel} ->
-			Server ! {Ref, ok}
-	after T * 1000 ->
-		if 	Next =:= [] ->
-			Server ! {done, S#state.name};
-			Next =/= [] ->
-			loop(S#state{to_go = Next})
-		end
-	end.
-	
-start(EventName, Delay) ->
-	spawn(?MODULE, init, [self(), EventName, Delay]).
-	
-strat_link(EventName,Delay) ->
-	spawn_link(?MODULE, init, [self(), EventName, Delay]).
+%%% Public interface
+start(EventName, DateTime) ->
+	io:format("event start~n"),
+    spawn(?MODULE, init, [self(), EventName, DateTime]).
 
-init(Server, EventName, DateTime) ->
-	loop(#state{server = Server,
-				name = EventName,
-				to_go = normalize(DeateTime)}).
+start_link(EventName, DateTime, Num_of_reminds, Remind_dates) ->
+	io:format("event start_link~n"),
+    spawn_link(?MODULE, init, [self(), EventName, DateTime, Num_of_reminds, Remind_dates]).
 
 cancel(Pid) ->
-	Ref = erlang:monitor(process, Pid),
-	Pid ! {self(), Ref, cancel},
-	receive
-		{Ref, ok} ->
-			erlang:demonitor(Ref, [flush]),
-			ok;
-		{'DOWN', Ref, process, Pid, _Reason} ->
-		ok
+	io:format("event cancel~n"),
+    %% Monitor in case the process is already dead
+    Ref = erlang:monitor(process, Pid),
+    Pid ! {self(), Ref, cancel},
+    receive
+        {Ref, ok} ->
+			io:format("event cancel receive ok~n"),
+            erlang:demonitor(Ref, [flush]),
+            ok;
+        {'DOWN', Ref, process, Pid, _Reason} ->
+			io:format("event cancel receive DOWN~n"),
+            ok
+    end.
+
+
+%%% Event's innards
+init(Server, EventName, DateTime, Num_of_reminds, Remind_dates) ->
+    io:format("event init~n"),
+	loop(#state{server=Server,
+                name=EventName,
+                to_go= DateTime,
+				num_of_reminds = Num_of_reminds,
+				remind_dates = reminders_time_to_go(Remind_dates,0)}).
+
+%% Loop uses a list for times in order to go around the ~49 days limit
+%% on timeouts.
+loop(S = #state{server=Server, to_go = DateTime, num_of_reminds = Num_of_reminds, remind_dates = [[H|T]|Next_reminder]}) ->
+    io:format("event loop~n"),
+	if 
+	Num_of_reminds > 0 ->
+		receive
+        {Server, Ref, cancel} ->
+			io:format("event loop receive cancel~n"),
+            Server ! {Ref, ok}
+		after H*1000 ->
+			if T =:= [] ->
+				io:format("event loop after na wysył remindera if~n"),
+				Server ! {reminder, S#state.name},
+				loop(S#state{num_of_reminds = Num_of_reminds - 1, remind_dates = Next_reminder});
+			   T =/= [] ->
+				io:format("event loop after na przejscie dalej w petli if~n"),
+				loop(S#state{ remind_dates = [[T]|Next_reminder]})
+			end
+		end;
+	Num_of_reminds == 0 ->
+	%% skończyłem na tym, że jakoś trzeba to go w obecnej formie zmienić tak, żeby
+	%% obliczało się DateTime dopiero jak przyjdzie na to czas
+		loop(S#state{num_of_reminds = -1, remind_dates = [time_to_go(DateTime)]});
+	Num_of_reminds == -1 ->
+		receive
+			{Server, Ref, cancel} ->
+				io:format("event loop receive cancel~n"),
+				Server ! {Ref, ok}
+		after H*1000 ->
+			if T =:= [] ->
+				io:format("event loop after wysyl info o koncu if~n"),
+				Server ! {done, S#state.name};
+			   T =/= [] ->
+				io:format("event loop after drugi if~n"),
+				loop(S#state{remind_dates=T})
+			end
+		end
 	end.
 
-time_to_go(TimeOut = {{_,_,_}, {_,_,_}}) ->
-	Now = calendar:local_time(),
-	ToGo = 	calendar:datetime_to_gregorian_seconds(TimeOut) -
-			calendar:datetime_to_gregorian_seconds(Now),
-	Secs = if 	ToGo > 0 -> ToGo;
+reminders_time_to_go([]) -> [];
+reminders_time_to_go(Remind_dates) ->
+	reminders_time_to_go(Remind_dates,0).
+reminders_time_to_go([],_)-> [[0,0],0];
+reminders_time_to_go([H|T],X) ->
+    io:format("event time_to_go~n"),
+	if 	X == 0 -> 
+		Now = calendar:local_time(),
+		ToGo = calendar:datetime_to_gregorian_seconds(H) -
+				calendar:datetime_to_gregorian_seconds(Now),
+		io:format("tutaj też ~n"),
+		Secs = if ToGo > 0  -> ToGo;
 				ToGo =< 0 -> 0
-			end,
-	normalize(Secs).
+				end,
+		[normalize(Secs)|reminders_time_to_go(T,H)];
+		X =/= 0 ->
+		Now = X,
+		ToGo = calendar:datetime_to_gregorian_seconds(H) -
+				calendar:datetime_to_gregorian_seconds(Now),
+		Secs = if ToGo > 0  -> ToGo;
+				ToGo =< 0 -> 0
+				end,
+		[normalize(Secs)|reminders_time_to_go(T,H)]
+	end.
 
+
+%%% private functions
+time_to_go(TimeOut={{_,_,_}, {_,_,_}}) ->
+    io:format("event time_to_go~n"),
+	Now = calendar:local_time(),
+    ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) -
+           calendar:datetime_to_gregorian_seconds(Now),
+	io:format("tutaj też ~n"),
+    Secs = if ToGo > 0  -> ToGo;
+              ToGo =< 0 -> 0
+           end,
+    normalize(Secs).
+
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used
 normalize(N) ->
-	Limit = 49*24*60*60,
-	[N rem Limit | lists: duplicate(N div Limit, Limit)].
+	io:format("event normalize~n"),
+    Limit = 49*24*60*60,
+    [N rem Limit | lists:duplicate(N div Limit, Limit)].
--- evserv.erl
+++ evserv.erl
@@ -1,91 +1,269 @@
+%% Event server
 -module(evserv).
 -compile(export_all).
--record(state, {events,
-				clients}).
-- record(event, {name="",
-				description="",
-				pid,
-				timeout={{1970,1,1},{0,0,0}}}).
 
-loop(S = #state{}) ->
-	receive
-	{Pid, MsgRef, {subscribe, Client}} ->
-		Ref = erlang:monitor(process,Client),
-		NewClients = orddict:store(Ref, Client, S#state.clients),
-		Pid ! {MsgRef, ok},
-		loop(S#state{clients=NewClients});
+-record(state, {events,    %% list of #event{} records
+                clients}). %% list of Pids
+-record(event, {name="",
+                description="",
+                pid,
+                timeout={{1970,1,1},{0,0,0}},
+				num_of_reminds = 0,
+				remind_dates=[]}).
 
-	{Pid, MsgRef, {add, Name, Description, TimeOut}} ->
-		case valid_datetime(TimeOut) of
-			true ->
-				EventPid = event:start_link(Name, TimeOut),
-				NewEvents  orddict:store(Name,
-										#event{ name = Name,
-												description = Description,
-												pid = EventPid,
-												timeout = TimeOut},
-										S#state.events),
-				Pid ! {MsgRef, ok},
-				loop(S#state{events = NewEvents});
-			false ->
-				Pid ! {MsgRef, {error, bad_timeout}},
-				loop(S)
-		end;
-		
-	{Pid, MsgRef, {cancel, Name}} ->
-		Events = case orddict:find(Name, S#state.events) of
-					{ok, E} ->
-						event:cancel(E#event.pid),
-						orddict:erase(Name,S#state.events);
-					error ->
-						S#state.events
-				end,
-		Pid ! {MsgRef, ok},
-		loop(S#state{events = Events});
+%%% User Interface
 
-	{done, Name} ->
-		case orddict:find(Name, S#state.events) of
-			{ok, E} ->
-				send_to_clients({done, E#event.name, E#event.description},
-								S#state.clients),
-				NewEvents = orddict:erase(Name, S#state.events),
-			error -> 
-				loop(S)
-		end;
 
-	shutdown ->
-		exit(shutdown);
+start() ->
+	io:format("evserv start~n"),
+    register(?MODULE, Pid=spawn(?MODULE, init, [])),
+    Pid.
 
-	{'DOWN', Ref, process, _Pid, _Reason} ->
-		loop(S#state{clients = orddict:erase(Ref, S#state.clients)});
+start_link() ->
+	io:format("evserv start_link~n"),
+    register(?MODULE, Pid=spawn_link(?MODULE, init, [])),
+    Pid.
 
-	code_change ->
-		?MODULE:loop(S);
+terminate() ->
+	io:format("evserv terminate~n"),
+    ?MODULE ! shutdown.
 
-	Unknown ->
-		io:format("Unknown message: ~p~n",[Unknown]),
-		loop(State)
-	end.
-	
 init() ->
-	loo(#state{events = orddict:new(),
-				clients = orddict:new()})
+	io:format("evserv init~n"),
+    %% Loading events from a static file could be done here.
+    %% You would need to pass an argument to init (maybe change the functions
+    %% start/0 and start_link/0 to start/1 and start_link/1) telling where the
+    %% resource to find the events is. Then load it from here.
+    %% Another option is to just pass the event straight to the server
+    %% through this function.
+    loop(#state{events=orddict:new(),
+                clients=orddict:new()}).
+
+subscribe(Pid) ->
+	io:format("evserv subscribe~n"),
+    Ref = erlang:monitor(process, whereis(?MODULE)),
+    ?MODULE ! {self(), Ref, {subscribe, Pid}},
+    receive
+        {Ref, ok} ->
+			io:format("evserv subscribe receive ok~n"),
+            {ok, Ref};
+        {'DOWN', Ref, process, _Pid, Reason} ->
+			io:format("evserv subscribe receive down~n"),
+            {error, Reason}
+    after 5000 ->
+		io:format("evserv subscribe after~n"),
+        {error, timeout}
+    end.
+%%add wywołuje error jako treść messga
+add_event(Name, Description, TimeOut, Num_of_reminds, Remind_dates) ->
+	X = lists:foldl(fun(_, Sum) -> 1 + Sum end, 0, Remind_dates),
+	if 	X == Num_of_reminds ->
+		Z = reminder_before_event(Remind_dates, TimeOut),
+		if Z == true ->
+			io:format("evserv add_event~n"),
+			Ref = make_ref(),
+			?MODULE ! {self(), Ref, {add, Name, Description, TimeOut, Num_of_reminds, Remind_dates}},
+			receive
+				{Ref, Msg} -> 
+				io:format("evserv add_event receive ~n"),
+				Msg
+			after 5000 ->
+				io:format("evserv add_event after~n"),
+				{error, timeout}
+			end;
+			Z == false ->
+			io:format("Co najmniej jedno przypomnienie ustawione jest po wydarzeniu. ~n"),
+			false
+		end;
+		X =/= Num_of_reminds ->
+		io:format("Liczba przypomnień nie zgadza się z zadeklarowaną wartością.~n"),
+		false
+	end.
+ %% add event 2 rozróżnia wiadomości, czy to error czy nie, i przy errorze wywołuje funkcję error
+add_event2(Name, Description, TimeOut) ->
+	io:format("evserv add_event2~n"),
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, {add, Name, Description, TimeOut}},
+    receive
+        {Ref, {error, Reason}} -> 
+		io:format("evserv add_event2 receive error~n"),
+		erlang:error(Reason);
+        {Ref, Msg} -> 
+		io:format("evserv add_event2 receive message~n"),
+		Msg
+    after 5000 ->
+		io:format("evserv add_event2 after~n"),
+        {error, timeout}
+    end.
+
+cancel(Name) ->
+	io:format("evserv cancel~n"),
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, {cancel, Name}},
+    receive
+        {Ref, ok} -> 
+		io:format("evserv cancel receive ok~n"),
+		ok
+    after 5000 ->
+		io:format("evserv cancel after~n"),
+        {error, timeout}
+    end.
+
+listen(Delay) ->
+	io:format("evserv listen~n"),
+    receive
+        M = {done, _Name, _Description} ->
+			io:format("evserv listen receive done~n"),
+            [M | listen(0)];
+		X = {reminder, _Name, _Description} ->
+			io:format("evserv listen receive reminder ~n"),
+			[X|listen(0)]
+    after Delay*1000 ->
+		io:format("evserv listen after~n"),
+        []
+    end.
+	
+%%% The Server itself
+
+loop(S=#state{}) ->
+	io:format("evserv loop~n"),
+
+    receive
+        {Pid, MsgRef, {subscribe, Client}} ->
+			io:format("evserv loop receive subscribe~n"),
+            Ref = erlang:monitor(process, Client),
+			%% Ref jest kluczem pod którym zapisywana jest wartosć Client w NewClients
+            NewClients = orddict:store(Ref, Client, S#state.clients),
+			%% Żeby dostać pid aktualnego klienta, musimy zrobić find(Ref,NewClients)
+			X = orddict:size(NewClients),
+            io:fwrite("wartos ~p~n",[X]),
+			Pid ! {MsgRef, ok},
+            loop(S#state{clients=NewClients});
+        {Pid, MsgRef, {add, Name, Description, TimeOut, Num_of_reminds, Remind_dates}} ->
+			io:format("evserv loop receive add~n"),
+            case (valid_datetime(TimeOut) and valid_reminders(Remind_dates)) of
+                true ->
+					io:format("evserv loop receive add true~n"),
+                    EventPid = event:start_link(Name, TimeOut, Num_of_reminds, Remind_dates),
+                    NewEvents = orddict:store(Name,
+                                              #event{name=Name,
+                                                     description=Description,
+                                                     pid=EventPid,
+                                                     timeout=TimeOut,
+													 num_of_reminds = Num_of_reminds,
+													 remind_dates = Remind_dates},
+                                              S#state.events),
+                    Pid ! {MsgRef, ok},
+                    loop(S#state{events=NewEvents});
+                false ->
+					io:format("evserv loop receive add false~n"),
+                    Pid ! {MsgRef, {error, bad_timeout}},
+                    loop(S)
+            end;
+        {Pid, MsgRef, {cancel, Name}} ->
+			io:format("evserv loop receive cancel~n"),
+            Events = case orddict:find(Name, S#state.events) of
+                         {ok, E} ->
+							io:format("evserv loop receive cancel case ok~n"),
+                             event:cancel(E#event.pid),
+                             orddict:erase(Name, S#state.events);
+                         error ->
+							io:format("evserv loop receive cancel error~n"),
+                             S#state.events
+                     end,
+            Pid ! {MsgRef, ok},
+            loop(S#state{events=Events});
+        {reminder, Name} ->
+			io:format("evserv loop receive reminder~n"),
+            case orddict:find(Name, S#state.events) of
+                {ok, E} ->
+				io:format("evserv loop receive reminder case ok~n"),
+                    send_to_clients({reminder, E#event.name, E#event.description},
+                                    S#state.clients),
+                    loop(S);
+                error ->
+					io:format("evserv loop receive reminder error~n"),
+                    %% This may happen if we cancel an event and
+                    %% it fires at the same time
+                    loop(S)
+            end;
+		{done, Name} ->
+			io:format("evserv loop receive done~n"),
+            case orddict:find(Name, S#state.events) of
+                {ok, E} ->
+				io:format("evserv loop receive done case ok~n"),
+                    send_to_clients({done, E#event.name, E#event.description},
+                                    S#state.clients),
+                    NewEvents = orddict:erase(Name, S#state.events),
+                    loop(S#state{events=NewEvents});
+                error ->
+					io:format("evserv loop receive done error~n"),
+                    %% This may happen if we cancel an event and
+                    %% it fires at the same time
+                    loop(S)
+            end;
+        shutdown ->
+			io:format("evserv loop receive shutdown~n"),
+            exit(shutdown);
+        {'DOWN', Ref, process, _Pid, _Reason} ->
+			io:format("evserv loop receive down~n"),
+            loop(S#state{clients=orddict:erase(Ref, S#state.clients)});
+        code_change ->
+			io:format("evserv loop receive code_change~n"),
+            ?MODULE:loop(S);
+        {Pid, debug} -> %% used as a hack to let me do some unit testing
+            io:format("evserv loop receive debug~n"),
+			Pid ! S,
+            loop(S);
+        Unknown ->
+            io:format("Unknown message: ~p~n",[Unknown]),
+            loop(S)
+    end.
+
+
+
+%%% Internal Functions
+send_to_clients(Msg, ClientDict) ->
+	io:format("evserv send to clients~n"),
+    orddict:map(fun(_Ref, Pid) -> Pid ! Msg end, ClientDict).
+
+valid_reminders([H|T]) ->
+	case valid_datetime(H) of
+	true -> valid_reminders(T);
+	false -> false
+	end;
+valid_reminders([]) -> true;
+valid_reminders(_) ->
+	false.
 
 valid_datetime({Date,Time}) ->
-	try
-		calendra:valid_date(Date) andalso valid_time(Time)
-	catch
-		error:function_clause ->
-			false
-	end;
-valid_datetime(_) ->
-	false.
-	
-valid_time({H,M,S}) -> valid_time(H,M,S).
-valid_time(H,M,S) when	H >= 0, H < 24,
-						M >= 0, M < 60,
-						S >= 0, S < 60 -> true;
-valid_time(_,_,_) -> false.
+	io:format("evserv valid_datetime~n"),
 
-send_to_clients(Msg, ClientDict) ->
-	orddict:map(fun(_Ref, Pid) -> Pid ! Msg end, ClientDict).
+    try
+        calendar:valid_date(Date) andalso valid_time(Time)
+    catch
+        error:function_clause -> %% not in {{Y,M,D},{H,Min,S}} format
+            false
+    end;		
+valid_datetime(_) ->
+    false.
+
+reminder_before_event(Remind_dates, DateTime) ->
+	X = lists:max(Remind_dates),
+	ToGo = calendar:datetime_to_gregorian_seconds(DateTime) -
+           calendar:datetime_to_gregorian_seconds(X),
+	if 	ToGo > 0 -> false;
+        ToGo =< 0 -> true
+    end.
+	
+
+%% calendar has valid_date, but nothing for days.
+%% This function is based on its interface.
+%% Ugly, but ugh.
+valid_time({H,M,S}) -> 
+	io:format("evserv valid time~n"),
+valid_time(H,M,S).
+valid_time(H,M,S) when H >= 0, H < 24,
+                       M >= 0, M < 60,
+                       S >= 0, S < 60 -> true;
+valid_time(_,_,_) -> false.
